dd9e1317ab0737a2f72a53154b90f17d44f05d2e
alexrc-dev <50207586+alexrc-dev@users.noreply.github.com>
2022-10-19 23:33:07 -0500

    Fix navbar and colors

--- settings.php
+++ settings.php
@@ -7,7 +7,7 @@ defined('MOODLE_INTERNAL') || die();
 if ($ADMIN->fulltree) {
 
     // Boost provides a nice setting page which splits settings onto separate tabs. We want to use it here.
-    $settings = new theme_boost_admin_settingspage_tabs('themesettingphoto', get_string('configtitle', 'theme_ridestur'));
+    $settings = new theme_boost_admin_settingspage_tabs('themesettingridestur', get_string('configtitle', 'theme_ridestur'));
 
     // Each page is a tab - the first is the "General" tab.
     $page = new admin_settingpage('theme_ridestur_general', get_string('generalsettings', 'theme_ridestur'));
